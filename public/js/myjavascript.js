88562ab35c116662d6efc1ba6d22c9e64844bb02
jhugh <jhugh@github.com>
2014-02-19 20:34:13 +0000

    added myjavascript.js, modified index.html

---
+++ public/js/myjavascript.js
@@ -0,0 +1,7 @@
+
+var doSum = function() {
+  var totalAmt = parseInt(document.getElementById("num1").value)+parseInt(document.getElementById("num2").value);
+  document.getElementById("tot").innerHTML = parseInt(totalAmt); 
+  return;
+}
+    
