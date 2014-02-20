e6d0d2941fee1c2192511fe03103798ca43b6a3b
jhugh <jhugh@github.com>
2014-02-20 10:42:09 +0000

    modified

--- public/js/myjavascript.js
+++ public/js/myjavascript.js
@@ -1,21 +1,21 @@
+//myjavascript.js
 
 var doSum = function() {
   var totalAmt = parseInt(document.getElementById("num1").value)+parseInt(document.getElementById("num2").value);
   document.getElementById("tot").innerHTML = parseInt(totalAmt); 
 }
 
+var firstObj = {
+   myName: "Jack"
+}
 
 var myObj = {
    myName: "John",
    sayHello: function() {
-     document.getElementById("testname").innerHTML = ("Hi! My name is " + myName );
-     //document.getElementById("testname").innerHTML = ("Hi! My name is Michael Caine");
+     document.getElementById("testname").innerHTML = ("Hi! My name is " + this.myName );
    }
 }
 
-//var nextObj = {
-//   myName: "Jack"
-//}
 
 
      
