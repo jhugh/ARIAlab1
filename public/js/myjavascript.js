eeb2fae5c1d69fb967b70d975e37c9ddedd20a93
jhugh <jhugh@github.com>
2014-02-20 10:40:04 +0000

    modified

--- public/js/myjavascript.js
+++ public/js/myjavascript.js
@@ -8,14 +8,14 @@ var doSum = function() {
 var myObj = {
    myName: "John",
    sayHello: function() {
-     document.getElementById("testname").innerHTML = ("Hi! My name is " + nextObj.myName );
+     document.getElementById("testname").innerHTML = ("Hi! My name is " + myName );
      //document.getElementById("testname").innerHTML = ("Hi! My name is Michael Caine");
    }
 }
 
-var nextObj = {
-   myName: "Jack"
-}
+//var nextObj = {
+//   myName: "Jack"
+//}
 
 
      
